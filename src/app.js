9dd431525a729ef260e978ac4ff264d05ba8fe08
miko <miko117123@gmail.com>
2020-12-16 17:30:40 +0800

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import { Component } from 'react'
+
+import 'taro-ui/dist/style/index.scss' // 全局引入一次即可
+import './app.styl'
+
+class App extends Component {
+
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentDidCatchError () {}
+
+  // this.props.children 是将要会渲染的页面
+  render () {
+    return this.props.children
+  }
+}
+
+export default App
