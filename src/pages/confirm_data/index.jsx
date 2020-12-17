e4599b418bdeeaec73f052e6165ebee506d65295
mikoZJ <miko117123@gmail.com>
2020-12-17 11:14:23 +0800

    测试跳转

--- src/pages/confirm_data/index.jsx
+++ src/pages/confirm_data/index.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
-import Taro from "@tarojs/taro";
 import { View, Text } from '@tarojs/components'
 import './index.styl'
 
 export default class Index extends Component {
+  constructor (props) {
+    super(props);
+  }
 
   componentWillMount () { }
 
@@ -15,17 +17,10 @@ export default class Index extends Component {
 
   componentDidHide () { }
 
-  clickHandler() {
-    // 跳转到目的页面，打开新页面
-    Taro.navigateTo({
-      url: '/pages/confirm_data/index'
-    })
-  }
-
   render () {
     return (
       <View className='index'>
-        <Text onClick={() => this.clickHandler()}>Hello world!</Text>
+        123
       </View>
     )
   }
